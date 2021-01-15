c0ade48ad5992e3773b47d9e6108a4e3dddd620c
Gautam Bhaskar <gautambhaskar@outlook.com>
2021-01-15 14:40:53 -0600

    changed auton distances

--- src/main/java/frc/robot/commands/Auton1.java
+++ src/main/java/frc/robot/commands/Auton1.java
@@ -20,6 +20,6 @@ public class Auton1 extends SequentialCommandGroup {
   public Auton1(Drivetrain x_Drive) {
     // Add your commands in the super() call, e.g.
     // super(new FooCommand(), new BarCommand());
-    super(new DriveSetDistance(x_Drive, 60), new TurnSetDegree(x_Drive, 180), new DriveSetDistance(x_Drive, 60));
+    super(new DriveSetDistance(x_Drive, 200), new TurnSetDegree(x_Drive, 180), new DriveSetDistance(x_Drive, 200));
   }
 }
